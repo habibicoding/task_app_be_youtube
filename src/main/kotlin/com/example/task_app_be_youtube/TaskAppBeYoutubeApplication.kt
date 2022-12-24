68b1b7a24284529e962ba6d57c331797cb2bcac8
habibicoding <habibicoding@yahoo.com>
2022-12-24 18:50:28 +0100

    Commit rest of project.

---
+++ src/main/kotlin/com/example/task_app_be_youtube/TaskAppBeYoutubeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.task_app_be_youtube
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskAppBeYoutubeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskAppBeYoutubeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/task_app_be_youtube/TaskAppBeYoutubeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.task_app_be_youtube
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskAppBeYoutubeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
