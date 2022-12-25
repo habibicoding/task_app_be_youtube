6ae5841a89da65310cc765b7abf55a17b1edde07
habibicoding <habibicoding@yahoo.com>
2022-12-25 18:23:41 +0100

    Add methods to fetch tasks.

--- src/main/kotlin/com/example/task_app_be_youtube/service/TaskService.kt
+++ src/main/kotlin/com/example/task_app_be_youtube/service/TaskService.kt
@@ -6,6 +6,7 @@ import com.example.task_app_be_youtube.data.model.TaskDto
 import com.example.task_app_be_youtube.exception.TaskNotFoundException
 import com.example.task_app_be_youtube.repository.TaskRepository
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 
 @Service
 class TaskService(private val repository: TaskRepository) {
@@ -31,6 +32,21 @@ class TaskService(private val repository: TaskRepository) {
             throw TaskNotFoundException("Task with the ID: $id does not exist!")
         }
     }
+
+    fun getTaskById(id: Long): TaskDto {
+        checkTaskForId(id)
+        val task: Task = repository.findTaskById(id)
+        return mappingEntityToDto(task)
+    }
+
+    fun getAllTasks(): List<TaskDto> =
+        repository.findAll().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
+
+    fun getAllOpenTasks(): List<TaskDto> =
+        repository.queryAllOpenTasks().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
+
+    fun getAllClosedTasks(): List<TaskDto> =
+        repository.queryAllClosedTasks().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
 
 
 }
