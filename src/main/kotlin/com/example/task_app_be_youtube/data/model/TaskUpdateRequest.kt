7100b0cde8444628fd97a36a1488476c257f96ab
habibicoding <habibicoding@yahoo.com>
2022-12-25 19:14:28 +0100

    Remove import.

--- src/main/kotlin/com/example/task_app_be_youtube/data/model/TaskUpdateRequest.kt
+++ src/main/kotlin/com/example/task_app_be_youtube/data/model/TaskUpdateRequest.kt
@@ -1,7 +1,6 @@
 package com.example.task_app_be_youtube.data.model
 
 import com.example.task_app_be_youtube.data.Priority
-import java.time.LocalDateTime
 
 data class TaskUpdateRequest(
     val description: String?,
