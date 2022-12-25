92e43a463f0eeea65b73d804a704ff3e2d6969bc
habibicoding <habibicoding@yahoo.com>
2022-12-25 19:14:46 +0100

    Add logic to create and update.

--- src/main/kotlin/com/example/task_app_be_youtube/service/TaskService.kt
+++ src/main/kotlin/com/example/task_app_be_youtube/service/TaskService.kt
@@ -3,10 +3,15 @@ package com.example.task_app_be_youtube.service
 import com.example.task_app_be_youtube.data.Task
 import com.example.task_app_be_youtube.data.model.TaskCreateRequest
 import com.example.task_app_be_youtube.data.model.TaskDto
+import com.example.task_app_be_youtube.data.model.TaskUpdateRequest
+import com.example.task_app_be_youtube.exception.BadRequestException
 import com.example.task_app_be_youtube.exception.TaskNotFoundException
 import com.example.task_app_be_youtube.repository.TaskRepository
 import org.springframework.stereotype.Service
+import org.springframework.util.ReflectionUtils
+import java.lang.reflect.Field
 import java.util.stream.Collectors
+import kotlin.reflect.full.memberProperties
 
 @Service
 class TaskService(private val repository: TaskRepository) {
@@ -48,5 +53,31 @@ class TaskService(private val repository: TaskRepository) {
     fun getAllClosedTasks(): List<TaskDto> =
         repository.queryAllClosedTasks().stream().map(this::mappingEntityToDto).collect(Collectors.toList())
 
+    fun createTask(request: TaskCreateRequest): TaskDto {
+        if (repository.doesDescriptionExist(request.description)) {
+            throw BadRequestException("There is already a task with the description: ${request.description}")
+        }
+        val task = Task()
+        mappingFromRequestToEntity(task, request)
+        val savedTask: Task = repository.save(task)
+        return mappingEntityToDto(savedTask)
+    }
 
+    fun updateTask(id: Long, request: TaskUpdateRequest): TaskDto {
+        checkTaskForId(id)
+        val existingTask: Task = repository.findTaskById(id)
+
+        for (prop in TaskUpdateRequest::class.memberProperties) {
+            if (prop.get(request) != null) {
+                val field: Field? = ReflectionUtils.findField(Task::class.java, prop.name)
+                field?.let {
+                    it.isAccessible = true
+                    ReflectionUtils.setField(it, existingTask, prop.get(request))
+                }
+            }
+        }
+
+        val savedTask: Task = repository.save(existingTask)
+        return mappingEntityToDto(savedTask)
+    }
 }
