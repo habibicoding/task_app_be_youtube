ae79a953d87481d0a465b28125dca73f7b331f94
habibicoding <habibicoding@yahoo.com>
2022-12-25 19:51:18 +0100

    Finish controller and add cors config.

--- src/main/kotlin/com/example/task_app_be_youtube/controller/TaskController.kt
+++ src/main/kotlin/com/example/task_app_be_youtube/controller/TaskController.kt
@@ -1,11 +1,18 @@
 package com.example.task_app_be_youtube.controller
 
+import com.example.task_app_be_youtube.data.model.TaskCreateRequest
 import com.example.task_app_be_youtube.data.model.TaskDto
+import com.example.task_app_be_youtube.data.model.TaskUpdateRequest
 import com.example.task_app_be_youtube.service.TaskService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -26,4 +33,18 @@ class TaskController(private val service: TaskService) {
     fun getTaskById(@PathVariable id: Long): ResponseEntity<TaskDto> =
         ResponseEntity(service.getTaskById(id), HttpStatus.OK)
 
+    @PostMapping("create")
+    fun createTask(
+        @Valid @RequestBody request: TaskCreateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.createTask(request), HttpStatus.OK)
+
+    @PatchMapping("updaet/{id}")
+    fun updateTask(
+        @PathVariable id: Long,
+        @Valid @RequestBody request: TaskUpdateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.updateTask(id, request), HttpStatus.OK)
+
+    @DeleteMapping("delete/{id}")
+    fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
+        ResponseEntity(service.deleteTask(id), HttpStatus.OK)
 }
