1060f5606e29f26b5b80615fdc20f818998d4f2f
habibicoding <habibicoding@yahoo.com>
2023-01-01 00:14:18 +0100

    Finish integration tests.

--- src/test/kotlin/com/example/task_app_be_youtube/controller/TaskControllerIntegrationTest.kt
+++ src/test/kotlin/com/example/task_app_be_youtube/controller/TaskControllerIntegrationTest.kt
@@ -1,6 +1,7 @@
 package com.example.task_app_be_youtube.controller
 
 import com.example.task_app_be_youtube.data.Priority
+import com.example.task_app_be_youtube.data.model.TaskCreateRequest
 import com.example.task_app_be_youtube.data.model.TaskDto
 import com.example.task_app_be_youtube.data.model.TaskUpdateRequest
 import com.example.task_app_be_youtube.exception.TaskNotFoundException
@@ -73,6 +74,65 @@ class TaskControllerIntegrationTest(@Autowired private val mockMvc: MockMvc) {
     }
 
     @Test
+    fun `given all opens tasks endpoint is called then check for number of tasks`() {
+        // given
+        val taskDto = TaskDto(
+            22,
+            "buy Hummus",
+            isReminderSet = false,
+            isTaskOpen = true,
+            createdOn = LocalDateTime.now(),
+            priority = Priority.MEDIUM
+        )
+        val tasks = listOf(dummyTaskDto, taskDto)
+
+        // when
+        `when`(mockService.getAllOpenTasks()).thenReturn(tasks)
+        val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/open-tasks"))
+
+        // then
+        resultActions.andExpect(MockMvcResultMatchers.status().`is`(200))
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(jsonPath("$.size()").value(tasks.size))
+    }
+
+    @Test
+    fun `given all closed tasks endpoint is called then check for number of tasks`() {
+        // given
+        val taskDto = TaskDto(
+            22,
+            "buy Hummus",
+            isReminderSet = false,
+            isTaskOpen = false,
+            createdOn = LocalDateTime.now(),
+            priority = Priority.MEDIUM
+        )
+        val tasks = listOf(dummyTaskDto, taskDto)
+
+        // when
+        `when`(mockService.getAllClosedTasks()).thenReturn(tasks)
+        val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/closed-tasks"))
+
+        // then
+        resultActions.andExpect(MockMvcResultMatchers.status().`is`(200))
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(jsonPath("$.size()").value(tasks.size))
+    }
+
+    @Test
+    fun `given one task when get task by id is called then check for correct description`() {
+        `when`(mockService.getTaskById(dummyTaskDto.id)).thenReturn(dummyTaskDto)
+        val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/task/${dummyTaskDto.id}"))
+
+        resultActions.andExpect(MockMvcResultMatchers.status().`is`(200))
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(jsonPath("$.id").value(dummyTaskDto.id))
+        resultActions.andExpect(jsonPath("$.description").value(dummyTaskDto.description))
+        resultActions.andExpect(jsonPath("$.isReminderSet").value(dummyTaskDto.isReminderSet))
+        resultActions.andExpect(jsonPath("$.isTaskOpen").value(dummyTaskDto.isTaskOpen))
+    }
+
+    @Test
     fun `when task id does not exist then except is not found response`() {
         `when`(mockService.getTaskById(taskId)).thenThrow(TaskNotFoundException("Task with id: $taskId does not exist!"))
         val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/task/$taskId"))
@@ -98,9 +158,8 @@ class TaskControllerIntegrationTest(@Autowired private val mockMvc: MockMvc) {
 
         `when`(mockService.updateTask(dummyTaskDto.id, request)).thenReturn(dummyTaskDto)
         val resultActions: ResultActions = mockMvc.perform(
-            MockMvcRequestBuilders.patch(
-                "/api/update/${dummyTaskDto.id}"
-            ).contentType(MediaType.APPLICATION_JSON)
+            MockMvcRequestBuilders.patch("/api/update/${dummyTaskDto.id}")
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(request))
         )
 
@@ -109,5 +168,40 @@ class TaskControllerIntegrationTest(@Autowired private val mockMvc: MockMvc) {
         resultActions.andExpect(jsonPath("$.description").value(dummyTaskDto.description))
         resultActions.andExpect(jsonPath("$.isReminderSet").value(dummyTaskDto.isReminderSet))
         resultActions.andExpect(jsonPath("$.isTaskOpen").value(dummyTaskDto.isTaskOpen))
+    }
+
+    @Test
+    fun `given create task request when task is created then check for the properties`() {
+        val request = TaskCreateRequest(
+            dummyTaskDto.description,
+            dummyTaskDto.isReminderSet,
+            dummyTaskDto.isTaskOpen,
+            LocalDateTime.now(),
+            dummyTaskDto.priority
+        )
+
+        `when`(mockService.createTask(request)).thenReturn(dummyTaskDto)
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(jsonPath("$.description").value(dummyTaskDto.description))
+        resultActions.andExpect(jsonPath("$.isReminderSet").value(dummyTaskDto.isReminderSet))
+        resultActions.andExpect(jsonPath("$.isTaskOpen").value(dummyTaskDto.isTaskOpen))
+    }
+
+    @Test
+    fun `given id for delete request when task is deleted then check for the response message`() {
+        val expectedMessage = "Task with the ID: $taskId has been deleted."
+
+        `when`(mockService.deleteTask(taskId)).thenReturn(expectedMessage)
+        val resultActions: ResultActions = mockMvc.perform(MockMvcRequestBuilders.delete("/api/delete/$taskId"))
+
+        resultActions.andExpect(MockMvcResultMatchers.status().`is`(200))
+        resultActions.andExpect(content().string(expectedMessage))
     }
 }
